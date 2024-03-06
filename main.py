5d75e1d604f968f2731e7121547882fe6e1c991a
PaigeBrown-27 <paige.brown@students.fhu.edu>
2024-03-06 13:32:25 -0600

    I only know parsing from my Greek friends

--- main.py
+++ main.py
@@ -3,14 +3,42 @@
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
 def parse_month(month):
-    pass
+    Jacob = {
+        "January" : "01",
+        "February" : "02",
+        "March" : "03",
+        "April" : "04",
+        "May" : "05",
+        "June" : "06",
+        "July" : "07",
+        "August" : "08",
+        "September" : "09",
+        "October" : "10",
+        "November" : "11",
+        "December" : "12"
+    }
+    return Jacob[month]
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
-    pass
+    Brooke = user_string.split()
+    Brookie = Brooke[0]
+    Texas_shaped_marshmallow = Brooke[1]
+    Bean = Brooke[2]
+    Ana = parse_month(Brookie)
+    if len(Texas_shaped_marshmallow) == 2:
+        Anthony = "0" + Texas_shaped_marshmallow[0:-1]
+
+    else:
+        Anthony = Texas_shaped_marshmallow[0:-1]
+    Clay = Ana + "/" + Anthony + "/" + Bean
+    return Clay
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+    Greek = input()
+    while Greek != -1:
+        print(parse_date(Greek))
+        Greek = input()
